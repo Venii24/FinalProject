9e6364bde7d6c2b36faf2532d9cbf52be43383c4
Venii24 <stefanieplankensteiner24@gmail.com>
2024-06-05 21:16:09 +0200

    merged Dialog and main

--- libs/obj/Debug/net8.0/libs.AssemblyInfo.cs
+++ libs/obj/Debug/net8.0/libs.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("libs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+771e6e04c732832bd66b3c22238329553d339ec8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+409e8924abc518c4da963e8141abbac78a8ac8d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("libs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("libs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
