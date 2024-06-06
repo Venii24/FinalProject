9d99a77760bc7c0042097b16fd30279bea4d9f23
Venii24 <stefanieplankensteiner24@gmail.com>
2024-06-06 10:28:22 +0200

    chnaged dialog text

--- libs/GameObjects/Helper.cs
+++ libs/GameObjects/Helper.cs
@@ -10,18 +10,18 @@ namespace libs
             Color = ConsoleColor.DarkGreen;
 
              //TODO Import and add those from JSON
-                    DialogNode node1 = new DialogNode("Hello, how can I help you?");
-                    DialogNode node2 = new DialogNode("Sure, what information do you need?");
-                    DialogNode node3 = new DialogNode("Sorry, I can't help with that.");
-                    DialogNode node4 = new DialogNode("Here is the information you requested.");
+                    DialogNode node1 = new DialogNode("Hello, do you need some help?");
+                    DialogNode node2 = new DialogNode("Sure, did you already find the key?");
+                    DialogNode node3 = new DialogNode("You should look for the key first. It is hidden in the room, then you can open the door.");
+                    DialogNode node4 = new DialogNode("Great! Look for the door and open it with the key.");
                     DialogNode node5 = new DialogNode("Goodbye!");
 
                     // Adding responses to nodes
-                    node1.AddResponse("I need some information.", node2);
-                    node1.AddResponse("Nothing, thanks.", node5);
+                    node1.AddResponse("Yes please, I do not know what to do", node2);
+                    node1.AddResponse("No everything is fine, thanks", node5);
 
-                    node2.AddResponse("Tell me more.", node4);
-                    node2.AddResponse("Nevermind.", node3);
+                    node2.AddResponse("Yes", node4);
+                    node2.AddResponse("No", node3);
 
                     node3.AddResponse("Okay, goodbye.", node5);
                     node4.AddResponse("Thanks!", node5);
--- libs/obj/Debug/net8.0/libs.AssemblyInfo.cs
+++ libs/obj/Debug/net8.0/libs.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("libs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+409e8924abc518c4da963e8141abbac78a8ac8d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e6364bde7d6c2b36faf2532d9cbf52be43383c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("libs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("libs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
