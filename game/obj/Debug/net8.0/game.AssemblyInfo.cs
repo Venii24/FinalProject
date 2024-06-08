3ebb0c6fc6cf692d1797b5636184f3ca5970dbcb
Venii24 <stefanieplankensteiner24@gmail.com>
2024-06-08 13:26:33 +0200

    adapted level 3 to make it harder

--- game/obj/Debug/net8.0/game.AssemblyInfo.cs
+++ game/obj/Debug/net8.0/game.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("game")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a267dafbc087c9c42d1fb8083e0a328455a62450")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+813978987a3db637929984ad90a281230d781f66")]
 [assembly: System.Reflection.AssemblyProductAttribute("game")]
 [assembly: System.Reflection.AssemblyTitleAttribute("game")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- libs/obj/Debug/net8.0/libs.AssemblyInfo.cs
+++ libs/obj/Debug/net8.0/libs.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("libs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a267dafbc087c9c42d1fb8083e0a328455a62450")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+813978987a3db637929984ad90a281230d781f66")]
 [assembly: System.Reflection.AssemblyProductAttribute("libs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("libs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
